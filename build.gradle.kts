c33dfdc790d088846124d986a95a2c1bdf4d47ab
Marcel Kliemannel <dev@marcelkliemannel.com>
2022-07-02 15:32:20 +0200

    Don't bundle Kotlin std library

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ fun properties(key: String) = project.findProperty(key).toString()
 plugins {
   java
   kotlin("jvm") version "1.5.10"
-  id("org.jetbrains.intellij") version "1.5.3"
+  id("org.jetbrains.intellij") version "1.6.0"
   id("com.github.johnrengelman.shadow") version "6.1.0"
   id("org.jetbrains.changelog") version "1.3.1"
 }
