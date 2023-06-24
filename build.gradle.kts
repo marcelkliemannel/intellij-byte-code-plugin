ab0f551629c7103214b1e04d958113caaa9287f5
Marcel Kliemannel <dev@marcelkliemannel.com>
2023-06-24 13:39:44 +0200

    Bump min version to 2023.1 to avoid plugin verifier problems

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,20 @@
 import org.jetbrains.changelog.Changelog
 import org.jetbrains.changelog.date
+import org.jetbrains.intellij.tasks.RunPluginVerifierTask.FailureLevel.COMPATIBILITY_PROBLEMS
+import org.jetbrains.intellij.tasks.RunPluginVerifierTask.FailureLevel.INTERNAL_API_USAGES
+import org.jetbrains.intellij.tasks.RunPluginVerifierTask.FailureLevel.INVALID_PLUGIN
+import org.jetbrains.intellij.tasks.RunPluginVerifierTask.FailureLevel.MISSING_DEPENDENCIES
+import org.jetbrains.intellij.tasks.RunPluginVerifierTask.FailureLevel.NON_EXTENDABLE_API_USAGES
+import org.jetbrains.intellij.tasks.RunPluginVerifierTask.FailureLevel.OVERRIDE_ONLY_API_USAGES
+import org.jetbrains.intellij.tasks.RunPluginVerifierTask.FailureLevel.SCHEDULED_FOR_REMOVAL_API_USAGES
 
 fun properties(key: String) = project.findProperty(key).toString()
 
 plugins {
   java
-  kotlin("jvm") version "1.7.21"
-  id("org.jetbrains.intellij") version "1.13.2"
+  // See bundled version: https://plugins.jetbrains.com/docs/intellij/using-kotlin.html#kotlin-standard-library
+  kotlin("jvm") version "1.8.0"
+  id("org.jetbrains.intellij") version "1.14.1"
   id("com.github.johnrengelman.shadow") version "8.1.0"
   id("org.jetbrains.changelog") version "2.0.0"
 }
@@ -18,7 +26,7 @@ repositories {
   mavenCentral()
 }
 
-val asm : Configuration by configurations.creating
+val asm: Configuration by configurations.creating
 
 val shadowAsmJar = tasks.create("shadowAsmJar", com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar::class) {
   group = "shadow"
@@ -67,6 +75,13 @@ tasks {
 
   runPluginVerifier {
     ideVersions.set(properties("pluginVerifierIdeVersions").split(',').map(String::trim).filter(String::isNotEmpty))
+    failureLevel.set(
+      listOf(
+        COMPATIBILITY_PROBLEMS, INTERNAL_API_USAGES, NON_EXTENDABLE_API_USAGES,
+        OVERRIDE_ONLY_API_USAGES, MISSING_DEPENDENCIES, SCHEDULED_FOR_REMOVAL_API_USAGES,
+        INVALID_PLUGIN
+      )
+    )
   }
 
   publishPlugin {
