e202dd644b0562b25231d6773e30e894c5ac424c
Marcel Kliemannel <dev@marcelkliemannel.com>
2026-05-18 19:58:57 +0200

    chore: Disable test parallelism for GitHub workflows

--- build.gradle.kts
+++ build.gradle.kts
@@ -151,7 +151,9 @@ tasks {
 
   withType<Test> {
     useJUnitPlatform()
-    maxParallelForks = (Runtime.getRuntime().availableProcessors() / 2).coerceIn(1, 2)
+    if (System.getenv("CI").isNullOrBlank()) {
+      maxParallelForks = (Runtime.getRuntime().availableProcessors() / 2).coerceIn(1, 2)
+    }
   }
 
   named("check") { dependsOn("spotlessCheck") }
